dc18fcefc0aca63074db20c5e8a2591841a8ab64
Paulo Gutiérrez <paulo.gutierrez.s.98@gmail.com>
2026-04-22 15:01:29 -0400

    Añade información proyectos + referencias para sección basica de contacto

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('Portafolio funcionando');
+
+const reveals = document.querySelectorAll('.reveal');
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+
+  reveals.forEach((el) => {
+    const elementTop = el.getBoundingClientRect().top;
+
+    if (elementTop < windowHeight - 100) {
+      el.classList.add('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', revealOnScroll);
+window.addEventListener('load', revealOnScroll);
